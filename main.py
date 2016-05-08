b3ad65851cb55b6293e0de4f7d5c17d5066bd989
Pablo Vicente <pablovicente93@usal.es>
2016-05-08 09:36:08 +0200

    Prediction with integer values

--- main.py
+++ main.py
@@ -89,3 +89,88 @@ plt.plot(GOLD_new['USD (AM)'], 'b')
 plt.plot(SILVER_new['USD'], 'g')
 plt.plot(PLAT_new['USD AM'], 'r')
 plt.show()
+
+
+#####################
+#  AUTOCORRELATION  #
+#####################
+
+x = [datetime.strptime(d,'%Y-%m-%d').date() for d in INDEX_DJIA['Date'].tolist()]
+nlag = len(x)
+
+acf_gold = correlation.autocorrelationf(GOLD['USD (AM)'], nlag, True)
+acf_silver = correlation.autocorrelationf(SILVER['USD'], nlag, True)
+acf_plat = correlation.autocorrelationf(PLAT['USD AM'], nlag, True)
+
+acf_djia = correlation.autocorrelationf(INDEX_DJIA['Close'], nlag, True)
+acf_hsi = correlation.autocorrelationf(INDEX_HSI['Close'], nlag, True)
+acf_ibex = correlation.autocorrelationf(INDEX_IBEX['Close'], nlag, True)
+acf_n225 = correlation.autocorrelationf(INDEX_N225['Close'], nlag, True)
+acf_sp500 = correlation.autocorrelationf(INDEX_SP500['Close'], nlag, True)
+                
+ccf = correlation.crosscorrelation(GOLD['USD (AM)'], GOLD['USD (AM)'], False)
+plt.rcParams["figure.figsize"] = fig_size
+pylab.figure(0)
+
+ax = plt.subplot(111)
+ax.plot(acf_gold, 'b', label='Gold')
+ax.plot(acf_silver, 'r', label='Silver')
+ax.plot(acf_plat, 'g', label='Plat')
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
+pylab.figure(1)
+
+ax = plt.subplot(111)
+ax.plot(acf_djia, 'b', label='DJIA')
+ax.plot(acf_hsi, 'r', label='HSI')
+ax.plot(acf_ibex, 'g', label='IBEX')
+ax.plot(acf_n225, 'y', label='N225')
+ax.plot(acf_sp500, 'k', label='SP500')
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
+######################
+#    SAVING DATA     #
+######################
+
+data_manipulation.write_csv_data(GOLD, '/Users/Pablo/Desktop/TFM/Data/GOLD.csv')
+data_manipulation.write_csv_data(SILVER, '/Users/Pablo/Desktop/TFM/Data/SILVER.csv')
+data_manipulation.write_csv_data(PLAT, '/Users/Pablo/Desktop/TFM/Data/PLAT.csv')
+data_manipulation.write_csv_data(INDEX_DJIA, '/Users/Pablo/Desktop/TFM/Data/INDEX_DJIA.csv')
+data_manipulation.write_csv_data(INDEX_HSI, '/Users/Pablo/Desktop/TFM/Data/INDEX_HSI.csv')
+data_manipulation.write_csv_data(INDEX_IBEX, '/Users/Pablo/Desktop/TFM/Data/INDEX_IBEX.csv')
+data_manipulation.write_csv_data(INDEX_N225, '/Users/Pablo/Desktop/TFM/Data/INDEX_N225.csv')
+data_manipulation.write_csv_data(INDEX_SP500, '/Users/Pablo/Desktop/TFM/Data/INDEX_SP500.csv')
+
+######################
+#  CROSSCORRELATION  #
+######################
+
+colors_ = list(six.iteritems(colors.cnames))
+x = [datetime.strptime(d,'%Y-%m-%d').date() for d in INDEX_DJIA['Date'].tolist()]
+pylab.figure(0)
+ax = plt.subplot(111)
+
+cross_correlations = []
+plots = []
+color_iter = iter(colors_)
+for i in range(1):
+    for j in range(len(values)):
+        if i != j:            
+            temp = correlation.crosscorrelation(values[i], values[j], True)
+            legend = str(values_names[i]) + ' ' + str(values_names[j])
+            cross_correlations.append(temp)  
+            color = next(color_iter)[0]
+            plots.append(ax.plot( temp, color, label=legend))
+            next(color_iter)
+            print("Cross correlation between %s and %s, color %s" % (values_names[i], values_names[j], color))
+
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+plt.show()
